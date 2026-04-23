b1d658c203b81c8c1c8a68d9be52bc2c602be977
David Rattray <david.rattray@gov.bc.ca>
2026-04-23 08:13:25 -0700

    Added source and binary packages
    Trying to get the restore to work properly on muon.

--- .Renviron.example.R
+++ .Renviron.example.R
@@ -1,3 +1,6 @@
 # Copy this to .Renviron and fill in your value
 # dev = targets Windfarm | prod = targets Dynamo
 ETL_ENV = dev
+
+# On internet-restricted machines, uncomment and set path to local package repo:
+# RENV_CONFIG_REPOS_OVERRIDE=file:///E:/Projects/packagerepo
