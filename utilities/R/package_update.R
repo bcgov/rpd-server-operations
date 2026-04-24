5d195e72576c4e9d8d364fa7a22d9e219b11d397
David Rattray <david.rattray@gov.bc.ca>
2026-04-24 10:19:00 -0700

    updating gitignore to remove .Rprofile

--- utilities/R/package_update.R
+++ utilities/R/package_update.R
@@ -33,7 +33,10 @@ install.packages(c(
 ))
 
 library(miniCRAN)
-
+pkgList <- c(
+  "igraph",
+  "bit64"
+)
 pkgList <- c(
   "arrow",
   "assertthat",
@@ -80,9 +83,22 @@ fullList <- pkgDep(pkgList, type = "win.binary", Rversion = getRversion())
 # )
 
 makeRepo(
-  fullList,
+  pkgList,
   path = "C:/Projects/packagerepo",
   download = TRUE,
   type = "win.binary",
   Rversion = getRversion()
 )
+
+# Attempts to get Muon sorted out
+# getOption("repos")
+# renv::config$repos.override("file:///E:/Projects/packagerepo")
+# options(renv.config.ppm.enabled = FALSE)
+# RENV_PATHS_CELLAR =  "E:/Projects/packagerepo/"
+# RENV_CONFIG_REPOS_OVERRIDE = "CRAN=file:///E:/Projects/packagerepo"
+# options(renv.config.repos.override = c(CRAN = "file:///E:/Projects/packagerepo"))
+
+# Adding the below to .Rprofile after source("renv/activate.R") has worked
+options(repos = c(LOCAL = "file:///E:/Projects/packagerepo"))
+# may need to call
+# renv::restore(repos = getOption("repos"))
