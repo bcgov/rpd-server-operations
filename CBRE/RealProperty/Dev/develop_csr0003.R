10decbc8ddf7637cc797ae70488be2034cd2127f
Rattray <David.Rattray@gov.bc.ca>
2026-09-08 16:13:25 -0700

    beginning development and identifying datasources

---
+++ CBRE/RealProperty/Dev/develop_csr0003.R
@@ -0,0 +1,68 @@
+# Load helper functions
+source(here::here("utilities/R/utilities.R"))
+
+# Set options
+options(scipen = 999)
+
+# Load libraries
+library(base64enc, quietly = TRUE, warn.conflicts = FALSE)
+library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
+library(here, quietly = TRUE, warn.conflicts = FALSE)
+library(httr2, quietly = TRUE, warn.conflicts = FALSE)
+library(jsonlite, quietly = TRUE, warn.conflicts = FALSE)
+library(lubridate, quietly = TRUE, warn.conflicts = FALSE)
+library(purrr, quietly = TRUE, warn.conflicts = FALSE)
+library(tibble, quietly = TRUE, warn.conflicts = FALSE)
+library(tidyr, quietly = TRUE, warn.conflicts = FALSE)
+library(stringr, quietly = TRUE, warn.conflicts = FALSE)
+library(openxlsx2, quietly = TRUE, warn.conflicts = FALSE)
+library(odbc, quietly = TRUE, warn.conflicts = FALSE)
+library(DBI, quietly = TRUE, warn.conflicts = FALSE)
+
+# For server logging
+# Begin timer
+task_start <- Sys.time()
+
+# Setup necessary variables
+ETL_STATUS <- "DEV"
+SQL_SERVER <- if (ETL_STATUS == "PROD") {
+  "dynamo.idir.bcgov\\CA_PRD"
+} else {
+  "windfarm.idir.bcgov\\CA_TST"
+}
+DB_NAME <- "BuildingIntelligence"
+SCHEMA_NAME <- "RealProperty"
+TABLE_NAME <- "CSR0003"
+TEMP_TABLE <- paste0("#", TABLE_NAME, "Temp")
+TARGET_TABLE <- DBI::Id(schema = SCHEMA_NAME, table = TABLE_NAME)
+SCRIPT_NAME <- "CSR0003"
+API_NAME <- "None"
+
+options(scipen = 999)
+options(digits = 7)
+
+# Connect to SQL database
+con <- dbConnect(
+  odbc(),
+  driver = "ODBC Driver 17 for SQL Server",
+  server = SQL_SERVER,
+  database = DB_NAME,
+  Trusted_Connection = "Yes"
+)
+
+# Query SQL Datasets ####
+query <- dbSendQuery(con, "SELECT * FROM CbreStaging.archibus_dp")
+DepartmentData <- dbFetch(query, n = -1)
+dbClearResult(query)
+
+query <- dbSendQuery(con, "SELECT * FROM CbreStaging.archibus_dv")
+DivisionData <- dbFetch(query, n = -1)
+dbClearResult(query)
+
+query <- dbSendQuery(con, "SELECT * FROM CbreStaging.pjm_dim_project")
+DimProjData <- dbFetch(query, n = -1)
+dbClearResult(query)
+
+# Dim proj column csf_migratedprojectid is the PIN number and will likely be the main linking piece of this work.
+# Then build on dv and dp tables, and looks like via agreement number link in property/building data
+# and tack on billing cost information from CSR0001
