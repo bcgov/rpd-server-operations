c4cc9de18d3cb7a6623bebae8606b6f38dd0344c
Rattray <David.Rattray@gov.bc.ca>
2026-09-08 15:51:05 -0700

    Last clean up

--- CBRE/Staging/archibus_dp.R
+++ CBRE/Staging/archibus_dp.R
@@ -31,8 +31,7 @@ con <- dbConnect(
 # call api
 raw_data <- call_cbre_api(
   CBRE_TABLE_NAME,
-  # start_time = etl_window$cbre_start_time,
-  start_time = "2020-01-01T00:00:00Z",
+  start_time = etl_window$cbre_start_time,
   end_time = etl_window$cbre_end_time
 )
 
